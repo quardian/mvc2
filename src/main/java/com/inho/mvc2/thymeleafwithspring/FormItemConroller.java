04811c6ae7a1abe985949b3aa2182c7d666a6baf
leeinho <leeinho@soleo.kr>
2021-12-07 09:25:15 +0900

    thymeleaf with spring - 빌드패턴을 통한 Model에 선택 추가

--- src/main/java/com/inho/mvc2/thymeleafwithspring/FormItemConroller.java
+++ src/main/java/com/inho/mvc2/thymeleafwithspring/FormItemConroller.java
@@ -6,10 +6,12 @@ import com.inho.mvc2.domain.Item;
 import com.inho.mvc2.domain.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.thymeleaf.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -26,6 +28,12 @@ public class FormItemConroller
 {
     private final ItemRepository itemRepository;
 
+
+    /**
+     * @ModelAttribute의 특별한 사용법
+     * 이 컨트롤러 호출시, 무조건 모델에 regions로 할당된다.
+     * @return
+     */
     @ModelAttribute("regions")
     public Map<String, String> regions() {
         Map<String, String> regions = new LinkedHashMap<>();
@@ -35,11 +43,19 @@ public class FormItemConroller
         return regions;
     }
 
+    /**
+     * 이 컨트롤러 호출시, 무조건 모델에 itemTypes로 할당된다.
+     * @return
+     */
     @ModelAttribute("itemTypes")
     public ItemType[] itemTypes() {
         return ItemType.values();
     }
 
+    /**
+     * 이 컨트롤러 호출시, 무조건 모델에 deliveryCodes로 할당된다.
+     * @return
+     */
     @ModelAttribute("deliveryCodes")
     public List<DeliveryCode> deliveryCodes() {
         List<DeliveryCode> deliveryCodes = new ArrayList<>();
@@ -49,8 +65,18 @@ public class FormItemConroller
         return deliveryCodes;
     }
 
+
+
     @GetMapping("/items")
     public String items(Model model) {
+
+        new Builder(model)
+                .setItemTypes("itemTypes1")
+                .setDeliveryCodes("deliveryCodes1")
+                .setRegions("regions1")
+                .set("user1", "value1")
+                .set("user2", "value2")
+                .build();
 
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
@@ -71,8 +97,8 @@ public class FormItemConroller
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
-
+    public String addItem(@ModelAttribute Item item,
+                          RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
         log.info("item.regions={}", item.getRegions());
         log.info("item.itemType={}", item.getItemType());
@@ -95,6 +121,51 @@ public class FormItemConroller
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
         return "redirect:/thymeleafwithsring/items/{itemId}";
+    }
+
+
+    public static class Builder
+    {
+        private final Model model;
+
+        public Builder(Model model){
+            this.model = model;
+        }
+
+        public Builder setRegions(@Nullable String key) {
+            if (StringUtils.isEmpty(key)) key = "regions";
+
+            Map<String, String> regions = new LinkedHashMap<>();
+            regions.put("SEOUL", "서울");
+            regions.put("BUSAN", "부산");
+            regions.put("JEJU", "제주");
+            model.addAttribute(key, regions);
+            return this;
+        }
+        public Builder setItemTypes(@Nullable  String key) {
+            if (StringUtils.isEmpty(key)) key = "itemTypes";
+            model.addAttribute(key, ItemType.values());
+            return this;
+        }
+
+        public Builder setDeliveryCodes(@Nullable  String key) {
+            if (StringUtils.isEmpty(key)) key = "deliveryCodes";
+            List<DeliveryCode> deliveryCodes = new ArrayList<>();
+            deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+            deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+            deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+            model.addAttribute(key, deliveryCodes);
+            return this;
+        }
+
+        public Builder set(String key, Object value) {
+            model.addAttribute(key, value);
+            return this;
+        }
+
+        public Model build(){
+            return model;
+        }
     }
 
 }
